af6694fb88feb2e748bacd5cf678cc2ae69475e1
Diulia C. Godoy Maciel <diuliac.pf@gmail.com>
2022-11-01 09:37:12 -0300

    add table test

--- table.py
+++ table.py
@@ -1,5 +1,16 @@
-from PyPDF2 import PdfReader
+# from PyPDF2 import PdfReader
+from PyPDF2 import PdfFileReader
 
-reader = PdfReader("anexos/Anexo_I.pdf")
-page = reader.pages[3]
-print(page.extract_text(0))
+
+# reader = PdfReader("anexos/Anexo_I.pdf")
+# page = reader.pages[3]
+# print(page.extract_text(0))
+
+pdfObject = open('./anexos/Anexo_I.pdf', 'rb')
+
+pdfReader = PdfFileReader(pdfObject)
+text=''
+for i in range(0,pdfReader.numPages):
+    pageObject = pdfReader.getPage(i)
+    text += pageObject.extractText()
+print(text)
