def7dc364741d8bd8c05914300a0bc68ec5b587f
Diulia C. Godoy Maciel <diuliac.pf@gmail.com>
2022-11-03 11:49:36 -0300

    add convert pdf files

---
+++ merged_csv.py
@@ -0,0 +1,39 @@
+# from PyPDF2 import PdfMerger
+from zipfile import ZipFile
+import os
+
+file_name = "merged_files.zip"
+# merger = PdfMerger()
+
+# for pdf in ["pdf2.pdf", "pdf3.pdf", "pdf4.pdf", "pdf5.pdf"]:
+#     merger.append(pdf)
+
+# merger.write("anexos/mergedpdf.pdf")
+# merger.close()
+
+def get_all_file_paths(directory):
+  
+    file_paths = []
+  
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+  
+    return file_paths 
+
+def main():
+    directory = '/'
+    file_paths = ["teste_diulia.csv"]
+
+    for file_name in file_paths:
+        print(file_name)
+  
+    with ZipFile('Teste_Diulia.zip','w') as zip:
+        for file in file_paths:
+            zip.write(file)
+  
+    print('All files zipped successfully!')
+
+if __name__ == "__main__":
+    main()
